c128f479695d7885bafdbc097c1809e943d1117b
emanuel-italo <emanuelitaloleal@hotmail.com>
2025-08-10 20:38:36 -0300

    alterar funcionario dentro da base

--- funcionario.py
+++ funcionario.py
@@ -25,10 +25,29 @@ def consultar_func(lista):
     codigo = input("Digite o codigo do funcionario: ")
     for funcionario in lista:
         if funcionario["codigo"] == codigo:
-            print(f"Funcionario encontrado: {funcionario}")
+            print(f"codigo encontrado: {funcionario}")
             return
         else:
-            print("produto não encontrado. ")
+            print("codigo não encontrado. ")
+
+def alterar_func(lista):
+    try:
+        codigo = input("Digite aqui a alteração que deseja fazer: ")
+        for funcionario in lista:
+            if funcionario ["codigo"] == codigo:
+                print("Funcionario encontrado. Digite os novos dados que deseja:")
+                funcionario["nome"] = input("Novo nome:  ")
+                funcionario["codigo"] = input("Novo codigo: ")
+                funcionario["idade"] = input ("Nova idade:")
+                funcionario["cargo"] = input("Novo cargo: ")
+                funcionario["salario"] = input("Novo salario: ")
+                print("Dados alterados com sucesso. ")
+                return
+        print("Funcionario não encontrado. ")
+    except ValueError:
+        print("Erro: digite valores numéricos válidos para idade, código e salário.")
+    finally:
+        print("Operação de alteração finalizada.\n")
 
 
 
